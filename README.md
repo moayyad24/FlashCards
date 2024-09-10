257ab2f36a546e7b4fd59424259a14d3ac99d0d0
moayyad <moayyadmmm@gmail.com>
2024-09-10 16:30:37 +0300

    remove buttom nav bar and add floating action button in home screen

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-#🎴 flashcards
+🎴#flashcards
 
 A powerful flashcard app that allows you to easily manage your flashcards and organize them into personalized groups.
 Keep important concepts fresh in your mind and ensure you never forget them.
 
 
-##🎨 System design
+🎨##System design
 
 ![flash_cards_design](https://github.com/user-attachments/assets/28afc806-883d-4426-80dd-da65328d54f7)
--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flashcards/core/theme/colors.dart';
 import 'package:flashcards/features/home/ui/widgets/card_list_tile.dart';
 import 'package:flashcards/features/home/ui/widgets/home_title.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +17,10 @@ class HomeScreen extends StatelessWidget {
               )),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
       body: const SafeArea(
           child: Column(
         children: [
@@ -28,54 +31,6 @@ class HomeScreen extends StatelessWidget {
           CardListTile(),
         ],
       )),
-      bottomNavigationBar: BottomAppBar(
-        color: AppColors.grey,
-        height: 75,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Ink(
-                  height: 30,
-                  width: 60,
-                  decoration: BoxDecoration(
-                      color: AppColors.cornflowerBlue.withOpacity(.2),
-                      borderRadius: BorderRadius.circular(30)),
-                  child: const Icon(
-                    Icons.home_outlined,
-                    color: AppColors.cornflowerBlue,
-                  ),
-                ),
-                const Text(
-                  'Home',
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: AppColors.cornflowerBlue),
-                )
-              ],
-            ),
-            const Column(
-              children: [
-                Icon(
-                  Icons.home_outlined,
-                ),
-                Text('home')
-              ],
-            ),
-            const Column(
-              children: [
-                Icon(
-                  Icons.home_outlined,
-                ),
-                Text('home')
-              ],
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
