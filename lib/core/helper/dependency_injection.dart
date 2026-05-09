74433155ea6271a4ce1ecc22ad18bc85a8d380b1
moayyad <moayyadmmm@gmail.com>
2026-05-09 15:24:09 +0300

    run the project after long time

--- lib/core/helper/dependency_injection.dart
+++ lib/core/helper/dependency_injection.dart
@@ -3,9 +3,10 @@ import 'package:cardy/features/home/data/repo/home_repo_impl.dart';
 import 'package:cardy/features/sets/data/repo/sets_repo_impl.dart';
 import 'package:cardy/features/settings/data/repo/settings_repo_impl.dart';
 import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
 
 final getIt = GetIt.instance;
-
+final logger = Logger(printer: PrettyPrinter(colors: true, printEmojis: true));
 void setupGetIt() {
   //Home
   getIt.registerLazySingleton<HomeRepoImpl>(() => HomeRepoImpl());
--- lib/core/helper/simple_bloc_observer.dart
+++ lib/core/helper/simple_bloc_observer.dart
@@ -1,28 +1,74 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:cardy/core/helper/dependency_injection.dart';
 
 class SimpleBlocObserver implements BlocObserver {
   @override
   void onChange(BlocBase bloc, Change change) {
-    debugPrint('onChange = $change');
+    logger.i('''
+    🔄 BLOC STATE CHANGE
+    Bloc: ${bloc.runtimeType}
+    Current State: ${change.currentState.runtimeType}
+    Next State: ${change.nextState.runtimeType}''');
   }
 
   @override
   void onClose(BlocBase bloc) {
-    debugPrint('onClose = $bloc');
+    logger.w('''
+    ⛔ BLOC CLOSED
+    Bloc: ${bloc.runtimeType}''');
   }
 
   @override
   void onCreate(BlocBase bloc) {
-    debugPrint('onCreate = $bloc');
+    logger.i('''
+    🆕 BLOC CREATED
+    Bloc: ${bloc.runtimeType}
+    Initial State: ${bloc.state.runtimeType}''');
   }
 
   @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {}
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    logger.e(
+      '''
+    ❌ BLOC ERROR
+    Bloc: ${bloc.runtimeType}
+    Error: $error''',
+      error: error,
+      stackTrace: stackTrace,
+    );
+  }
 
   @override
-  void onEvent(Bloc bloc, Object? event) {}
+  void onEvent(Bloc bloc, Object? event) {
+    logger.d('''
+    🎫 BLOC EVENT
+    Bloc: ${bloc.runtimeType}
+    Event: ${event.runtimeType}
+    $event''');
+  }
 
   @override
-  void onTransition(Bloc bloc, Transition transition) {}
+  void onTransition(Bloc bloc, Transition transition) {
+    logger.t('''
+    🔄 BLOC TRANSITION
+    Bloc: ${bloc.runtimeType}
+    Event: ${transition.event.runtimeType}
+    Current State: ${transition.currentState.runtimeType}
+    Next State: ${transition.nextState.runtimeType}''');
+  }
+
+  @override
+  void onDone(
+    Bloc bloc,
+    Object? event, [
+    Object? error,
+    StackTrace? stackTrace,
+  ]) {
+    logger.t('''
+    ✅ BLOC DONE
+    Bloc: ${bloc.runtimeType}
+    Event: ${event?.runtimeType}
+    Error: ${error?.toString() ?? 'None'}
+    StackTrace: ${stackTrace ?? 'None'}''');
+  }
 }
