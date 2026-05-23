4aeb3999b84ca733d44e2b107fbb40c0d250c606
moayyad <moayyadmmm@gmail.com>
2026-05-23 15:07:55 +0300

    add upgrader package to notify users for new update available

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -12,6 +12,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:upgrader/upgrader.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -29,57 +30,61 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const HomeAppBar(),
-      floatingActionButton: MyFloatingActionButton(onTap: () {
-        Navigator.pushNamed(context, Routes.addFolderSetScreen);
-      }),
-      bottomNavigationBar: BlocBuilder<AdsCubit, AdsState>(
-        builder: (context, state) {
-          if (state.isBannerSuccess &&
-              context.read<AdsCubit>().banner != null) {
-            return SafeArea(
-              child: SizedBox(
-                width: context.read<AdsCubit>().banner!.size.width.toDouble(),
-                height: context.read<AdsCubit>().banner!.size.height.toDouble(),
-                child: AdWidget(ad: context.read<AdsCubit>().banner!),
-              ),
-            );
-          }
-          return const SizedBox.shrink();
-        },
-      ),
-      body: SafeArea(
-        child: BlocBuilder<HomeCubit, HomeState>(
+    return UpgradeAlert(
+      child: Scaffold(
+        appBar: const HomeAppBar(),
+        floatingActionButton: MyFloatingActionButton(onTap: () {
+          Navigator.pushNamed(context, Routes.addFolderSetScreen);
+        }),
+        bottomNavigationBar: BlocBuilder<AdsCubit, AdsState>(
           builder: (context, state) {
-            if (state is HomeFetchLoading) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (state is HomeFetchSuccess) {
-              if (state.homeData.folders.isEmpty &&
-                  state.homeData.sets.isEmpty) {
-                return const EmptyPlaceholder();
-              }
-              return SingleChildScrollView(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20).r,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (state.homeData.folders.isNotEmpty) ...[
-                      FolderCardList(folders: state.homeData.folders),
-                      20.verticalSpace,
-                    ],
-                    if (state.homeData.sets.isNotEmpty)
-                      SetsList(sets: state.homeData.sets),
-                  ],
+            if (state.isBannerSuccess &&
+                context.read<AdsCubit>().banner != null) {
+              return SafeArea(
+                child: SizedBox(
+                  width: context.read<AdsCubit>().banner!.size.width.toDouble(),
+                  height:
+                      context.read<AdsCubit>().banner!.size.height.toDouble(),
+                  child: AdWidget(ad: context.read<AdsCubit>().banner!),
                 ),
               );
-            } else {
-              return const Center(
-                child: Text('There was an error'),
-              );
             }
+            return const SizedBox.shrink();
           },
+        ),
+        body: SafeArea(
+          child: BlocBuilder<HomeCubit, HomeState>(
+            builder: (context, state) {
+              if (state is HomeFetchLoading) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (state is HomeFetchSuccess) {
+                if (state.homeData.folders.isEmpty &&
+                    state.homeData.sets.isEmpty) {
+                  return const EmptyPlaceholder();
+                }
+                return SingleChildScrollView(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20)
+                          .r,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      if (state.homeData.folders.isNotEmpty) ...[
+                        FolderCardList(folders: state.homeData.folders),
+                        20.verticalSpace,
+                      ],
+                      if (state.homeData.sets.isNotEmpty)
+                        SetsList(sets: state.homeData.sets),
+                    ],
+                  ),
+                );
+              } else {
+                return const Center(
+                  child: Text('There was an error'),
+                );
+              }
+            },
+          ),
         ),
       ),
     );
