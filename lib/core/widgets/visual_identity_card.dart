ac5b0259fa1eff367aaea1eb399b4a8e400c8710
moayyad <moayyadmmm@gmail.com>
2026-06-05 00:23:24 +0300

    remove the icon picker package and build a normal icon picker dialog

--- lib/core/widgets/visual_identity_card.dart
+++ lib/core/widgets/visual_identity_card.dart
@@ -3,8 +3,6 @@ import 'package:cardy/core/theme/app_text_styles.dart';
 import 'package:cardy/core/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
-import 'package:flutter_iconpicker/Models/configuration.dart';
-import 'package:flutter_iconpicker/flutter_iconpicker.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class VisualIdentityCard extends StatefulWidget {
@@ -30,7 +28,38 @@ class _VisualIdentityCardState extends State<VisualIdentityCard> {
   ];
 
   int _selectedColorIndex = 0;
-  IconData _selectedIcon = Icons.grid_view_rounded;
+  IconData _selectedIcon = Icons.library_books_rounded;
+
+  final List<IconData> _learningIcons = [
+    Icons.school_rounded,
+    Icons.book_rounded,
+    Icons.menu_book_rounded,
+    Icons.local_library_rounded,
+    Icons.library_books_rounded,
+    Icons.science_rounded,
+    Icons.calculate_rounded,
+    Icons.language_rounded,
+    Icons.history_edu_rounded,
+    Icons.computer_rounded,
+    Icons.architecture_rounded,
+    Icons.psychology_rounded,
+    Icons.auto_stories_rounded,
+    Icons.biotech_rounded,
+    Icons.functions_rounded,
+    Icons.translate_rounded,
+    Icons.lightbulb_rounded,
+    Icons.quiz_rounded,
+    Icons.grade_rounded,
+    Icons.assignment_rounded,
+    Icons.workspace_premium_rounded,
+    Icons.model_training_rounded,
+    Icons.tips_and_updates_rounded,
+    Icons.class_rounded,
+    Icons.chrome_reader_mode_rounded,
+    Icons.featured_play_list_rounded,
+    Icons.verified_rounded,
+    Icons.cast_for_education_rounded,
+  ];
 
   Color get _currentValuesColor => _colorPresets[_selectedColorIndex];
 
@@ -74,28 +103,74 @@ class _VisualIdentityCardState extends State<VisualIdentityCard> {
     );
   }
 
-  // Helper method to trigger flutter_iconpicker
+  // Helper method to trigger custom icon picker dialog
   Future<void> _pickIcon() async {
-    IconPickerIcon? icon = await showIconPicker(
-      context,
-      configuration: SinglePickerConfiguration(
-        title: Text(
-          'Pick an icon',
-          style: AppTextStyles.bold18,
-        ),
-        iconPickerShape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-        searchComparator: (String search, IconPickerIcon icon) =>
-            search
-                .toLowerCase()
-                .contains(icon.name.replaceAll('_', ' ').toLowerCase()) ||
-            icon.name.toLowerCase().contains(search.toLowerCase()),
-      ),
+    IconData? icon = await showDialog<IconData>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: AppColors.darkblue10131A,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          title: Text(
+            'Pick an icon',
+            style: AppTextStyles.bold18.copyWith(color: Colors.white),
+          ),
+          content: SizedBox(
+            width: double.maxFinite,
+            height: 200.h,
+            child: GridView.builder(
+              shrinkWrap: true,
+              itemCount: _learningIcons.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                crossAxisSpacing: 16.w,
+                mainAxisSpacing: 16.h,
+              ),
+              itemBuilder: (context, index) {
+                final currentIcon = _learningIcons[index];
+                final isSelected = currentIcon == _selectedIcon;
+                return GestureDetector(
+                  onTap: () => Navigator.pop(context, currentIcon),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: isSelected
+                          ? _currentValuesColor.withAlpha(51) // ~20% opacity
+                          : Colors.transparent,
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                        color: isSelected
+                            ? _currentValuesColor
+                            : AppColors.border424754,
+                        width: 1.5,
+                      ),
+                    ),
+                    child: Icon(
+                      currentIcon,
+                      color: isSelected ? _currentValuesColor : Colors.white70,
+                      size: 28.w,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text(
+                'Cancel',
+                style: AppTextStyles.medium16.copyWith(color: Colors.white70),
+              ),
+            ),
+          ],
+        );
+      },
     );
 
     if (icon != null) {
       setState(() {
-        _selectedIcon = icon.data;
+        _selectedIcon = icon;
       });
       widget.onIdentityChanged(_currentValuesColor, _selectedIcon);
     }
